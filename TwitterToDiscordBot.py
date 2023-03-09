6429a532a08329d4f3fceecf93d818a1aa2eb9b5
PeterGarvanski <117472818+PeterGarvanski@users.noreply.github.com>
2023-03-09 16:49:20 +0000

    reworked
    reworked so it doesnt require api keys

--- TwitterToDiscordBot.py
+++ TwitterToDiscordBot.py
@@ -1,30 +1,40 @@
-import tweepy
+import snscrape.modules.twitter as scraper
 import json
-import configparser
+import datetime
+import re
 
-# Read Configs
-config = configparser.ConfigParser()
-config.read("config.ini")
+def main():
+    with open("twitter.json") as file:
+        jsonData = json.load(file)
+    
+    time = datetime.datetime.now() - datetime.timedelta(minutes=30)
+    currentDate = re.search(r"(\d{2}):(\d{2}):(\d{2})", str(time))
+    
+    hours = int(currentDate.group(1))
+    minutes = int(currentDate.group(2))
+    seconds = int(currentDate.group(3))
+    
+    customTime = (hours * 3600) + (minutes * 60) + (seconds)
 
-api_key = config["twitter"]["api_key"]
-api_key_secret = config["twitter"]["api_key_secret"]
+    tweets = []
+    running = True
+    
+    while running <= 2:
+        for user in jsonData["userTag"]:
+            print(user)
+            for query in jsonData["query"]:
+                q = f"({query} (from:{user}) since:{datetime.date.today()})"
+                for tweet in scraper.TwitterSearchScraper(q).get_items():
+                    tweetDate = re.search(r"(\d{2}):(\d{2}):(\d{2})", str(tweet.date))
+                    h = int(tweetDate.group(1))
+                    m = int(tweetDate.group(2))
+                    s = int(tweetDate.group(3))
+                    tweetTime = (h * 3600) + (m * 60) + s
+                    if tweetTime > customTime:
+                        print(tweet.url)
 
-access_token = config["twitter"]["access_token"]
-access_token_secret = config["twitter"]["access_token_secret"]
+        if len(tweets) > 0:
+            running = False
 
-# Authentication
-authentication = tweepy.OAuthHandler(api_key, api_key_secret)
-authentication.set_access_token(access_token, access_token_secret)
-
-# API Instance
-API = tweepy.API(authentication)
-
-with open("twitter.json") as file:
-    jsonData = json.load(file)
-
-for user in jsonData["userTag"]:
-    tweets = API.user_timeline(user)
-    for tweet in tweets:
-        for query in jsonData["query"]:
-            if query in tweet:
-                print(tweet)
+if __name__ == "__main__":
+    main()
